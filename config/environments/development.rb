4b51c652a88d2a0104973a46cc4e544d2bb162ee
Marco <m.o.g2656@gmail.com>
2024-07-22 23:28:34 -0400

    feat: add layout for Auth + basic navigation and sidebar

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,9 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # https://github.com/rails/rails/issues/44185?ref=nelson.cloud
+  config.sign_out_via = :get
 end
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,11 +12,24 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      minHeight: {
+        'content': 'calc(100vh - 64px)'
+      }
     },
   },
   plugins: [
+    require("daisyui"),
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/container-queries'),
-  ]
+  ],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+    darkTheme: "dark", // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+  },
 }
